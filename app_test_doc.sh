a7177ffb66ca143a55423f8b522e64836459c670
José Renato Ferreira <joserenatosp@outlook.com>
2019-06-26 19:45:11 -0300

    Envio teste branch publica

---
+++ app_test_doc.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+
+echo "qualquer coisa"
+
+OCEAN=DADOS
+echo "OLÁ SOU $OCEAN"
+
+echo "Esses são os meus arquivos: $DIRETORIO "
+DIRETORIO=bash ls -l
+
+--echo "Esses são os meus arquivos: $DIRETORIO "
+
+
