3eb3e71c2cc85e900aee193ca0373143b4ac393e
José Renato Ferreira <joserenatosp@outlook.com>
2019-06-26 19:51:29 -0300

    TEste envio amigo

--- app_test_doc.sh
+++ app_test_doc.sh
@@ -3,5 +3,11 @@
 echo "qualquer coisa"
 
 OCEAN=DADOS
+echo "OLÁ SOU $OCEAN"
 
-echo $OCEAN
+echo "Esses são os meus arquivos: $DIRETORIO "
+DIRETORIO=bash ls -l
+
+--echo "Esses são os meus arquivos: $DIRETORIO "
+
+
